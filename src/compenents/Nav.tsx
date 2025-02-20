8edd38804a8081df0b166a9e4a421d43cec20639
rbdevelopment-canada <rb.development@outlook.com>
2025-02-20 12:40:38 -0500

    Created the pages, components, and css folders and moved their respective files

---
+++ src/compenents/Nav.tsx
@@ -0,0 +1,7 @@
+const Nav = () => {
+    return ( 
+        <h1>NAV</h1>
+     );
+}
+ 
+export default Nav;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Nav from "../compenents/Nav";
+
+
+const Home = () => {
+    return ( 
+        <>
+            <h1>HOME</h1>
+            <Nav />
+        </>
+     );
+}
+ 
+export default Home;
+
